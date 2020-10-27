c9b5bcfa80a1636d49e38a5f3c613e90bc3f6820
Sword-CS <1144975679@qq.com>
2020-10-27 09:01:41 +0800

    原始版本

---
+++ HelloGit/src/main/java/com/csj/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.csj.git;
+
+/**
+ * @Author : Sword
+ * @Date : 2020/10/26 0026
+ * @Time : 上午 1:28
+ * @File : HelloGit.java
+ * @Describe :
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
